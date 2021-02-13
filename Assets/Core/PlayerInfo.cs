a45ef23d0bebdec039607afcb84be388388618da
Alex Mair <alex@theshrine.ca>
2021-02-13 11:31:52 -0600

    Temp genesis until i get my shit together

---
+++ Assets/Core/PlayerInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public string Name = "unnamed player";
+    public int Credits = 0;
+    // todo: rank, ship id, etc.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UI/MainMenuResponder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceJunk.UI
+{
+
+    public class MainMenuResponder : MonoBehaviour
+    {
+        public void OnContinuePress()
+        {
+            print("i am bender please insert girder");
+        }
+
+        public void OnNewGamePress()
+        {
+            print("hooray you want a new game");
+        }
+
+        public void OnLoadGamePress()
+        {
+            print("now you want to LOAD a GAME?");
+        }
+
+        public void OnOptionsPress()
+        {
+        }
+
+        public void OnQuitPress()
+        {
+            Debug.Log("goodbye");
+            Application.Quit();
+        }
+    }
+
+}
