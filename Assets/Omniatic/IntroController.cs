58e0afbb05a8ff2050d0e772d317cf30daab5511
Alex Mair <alex@theshrine.ca>
2021-03-06 12:01:48 -0600

    trying to do the intro stuff

--- Assets/Omniatic/IntroController.cs
+++ Assets/Omniatic/IntroController.cs
@@ -6,16 +6,67 @@ using UnityEngine.SceneManagement;
 
 namespace Omniatic
 {
+    // this code is garbage and should probably be refactored into something better
     public class IntroController : MonoBehaviour
     {
-        public float introDelaySeconds = 2f;
+        public GameObject buddyObject;
+        public GameObject logoObject;
 
-        // Start is called before the first frame update
+        protected readonly float fadeInRate = 0.5f;
+        protected readonly float fadeOutRate = 0.8f;
+        protected readonly float blinkDelay = 0.3f;
+
+        protected SpriteRenderer buddySprite;
+
+        public Sprite lookLeft;
+        public Sprite lookRight;
+        public Sprite lookCentre;
+        public Sprite blink;
+
         void Start()
         {
+            buddySprite = buddyObject.GetComponent<SpriteRenderer>();
+
             // TODO: if global options set to skip intro then Transition()
 
-            StartCoroutine(LogoFadeout());
+            StartCoroutine(AnimationGo());
+        }
+
+        IEnumerator AnimationGo()
+        {
+            yield return new WaitForSeconds(fadeInRate);
+
+            Blink();
+            yield return new WaitForSeconds(blinkDelay);
+            LookLeft();
+            yield return new WaitForSeconds(blinkDelay);
+
+            Blink();
+            yield return new WaitForSeconds(blinkDelay);
+            LookRight();
+            yield return new WaitForSeconds(blinkDelay);
+
+            Blink();
+            yield return new WaitForSeconds(blinkDelay);
+            LookLeft();
+            yield return new WaitForSeconds(blinkDelay);
+
+            Blink();
+            yield return new WaitForSeconds(blinkDelay);
+            LookCentre();
+            yield return new WaitForSeconds(blinkDelay * 4);
+
+            LookRight();
+            yield return new WaitForSeconds(blinkDelay);
+
+            StartCoroutine(Fadeout());
+        }
+
+        IEnumerator Fadeout()
+        {
+            yield return new WaitForSeconds(fadeOutRate);
+
+            Transition();
         }
 
         /// <summary>
@@ -26,17 +77,34 @@ namespace Omniatic
             Transition();
         }
 
-        IEnumerator LogoFadeout()
-        {
-            yield return new WaitForSeconds(introDelaySeconds);
-
-            Transition();
-        }
-
         protected void Transition()
         {
             SceneManager.LoadScene("MainMenu");
         }
+
+        #region buddy animations
+
+        void Blink()
+        {
+            buddySprite.sprite = blink;
+        }
+
+        void LookCentre()
+        {
+            buddySprite.sprite = lookCentre;
+        }
+
+        void LookLeft()
+        {
+            buddySprite.sprite = lookLeft;
+        }
+
+        void LookRight()
+        {
+            buddySprite.sprite = lookRight;
+        }
+
+        #endregion buddy
     }
 
 }
