ea5841978463ee0d8d9e0b4e211d66e313582ab6
Alex Mair <alex@theshrine.ca>
2021-03-31 12:32:10 -0600

    trying out the new game dialog

---
+++ Assets/UI/NewGameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceJunk.UI
+{   
+    public class NewGameController : MonoBehaviour
+    {
+        public GameObject captainGameInput;
+        public GameObject difficultyDropdown;
+        public GameObject enableTutorialToggle;
+            
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+
+}
